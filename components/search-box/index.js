dc016efc6a64bd78f62136aba17a899bed3727b1
Mel-A <meabuin@alumno.unlam.edu.ar>
2020-09-29 16:32:51 -0300

    adding submit action with params

--- components/search-box/index.js
+++ components/search-box/index.js
@@ -18,16 +18,25 @@ const SearchBox = () => {
 
   const [ pokemon, setPokemon ] = useState(null)
   const [ getPokemon, { loading, data } ] = useLazyQuery(GET_POKEMON_INFO_BY_NAME, {
-    onCompleted: data => setPokemon(data.pokemon)
+    onCompleted: data => setPokemon(data.pokemon),
+    fetchPolicy: 'network-only'
   })
 
+  const handleSubmit = event => {
+    event.preventDefault()
+    const form = event.target.parentElement
+    const name = form.elements.name.value
+    name && getPokemon({ variables: { name: `pokemon/${name}` } })
+  }
 
-  console.log(pokemon)
   return (
     <div>
-      <input name='name' placeholder='type pokemon name' />
-      <button onClick={() => getPokemon({ variables: { name: 'pokemon/ditto' } })} >Search</button>
-    </div>);
+      <form>
+        <input name='name' placeholder='type pokemon name' />
+        <button type='button' onClick={e => handleSubmit(e)} >Search</button>
+      </form><p>{pokemon && pokemon.name}</p>
+    </div>
+  )
 }
 
 export default SearchBox
