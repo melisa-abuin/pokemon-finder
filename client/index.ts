02ba066b2f4052c9bcb25027e8d8dbd850b33b25
melisa-abuin <melisa.abuin@gmail.com>
2023-06-13 22:33:17 +0200

    feature #23: migrate app to typescript

---
+++ client/index.ts
@@ -0,0 +1,11 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { RestLink } from 'apollo-link-rest'
+
+const restLink = new RestLink({
+  uri: 'https://pokeapi.co/api/v2/',
+})
+
+export const client = new ApolloClient({
+  link: restLink,
+  cache: new InMemoryCache(),
+})
---
+++ components/search-box/api.ts
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import { useLazyQuery } from '@apollo/client'
+import gql from 'graphql-tag'
+import { Pokemon } from '@/interfaces/pokemon'
+
+const GET_POKEMON_INFO_BY_NAME = gql`
+  query getPokemon($name: String!) {
+    pokemon @rest(type: "Pokemon", path: $name) {
+      name
+      abilities
+      image: sprites {
+        front: front_default
+      }
+      types {
+        type
+      }
+    }
+  }
+`
+export const useApi = () => {
+  const [pokemon, setPokemon] = useState<Pokemon | null>(null)
+  const [getPokemon, { loading, called }] = useLazyQuery(
+    GET_POKEMON_INFO_BY_NAME,
+    {
+      onCompleted: (data) => setPokemon(data.pokemon),
+      fetchPolicy: 'network-only',
+    }
+  )
+
+  const setData = (name: string) => {
+    name && getPokemon({ variables: { name: `pokemon/${name}` } })
+  }
+
+  return { pokemon, called, loading, setData }
+}
