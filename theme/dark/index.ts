62e2a6a7773c74ae7e07d4026f82d828885e0cb4
melisa-abuin <melisa.abuin@gmail.com>
2023-08-01 22:55:22 +0200

    feature #37: create dark and light themes

---
+++ theme/dark/index.ts
@@ -0,0 +1,33 @@
+const darkTheme = {
+  colors: {
+    alabaster: '#FAFAFA',
+    astral: '#327DA0',
+    aquamarine: '#A1DAD7',
+    azure: '#0080FF',
+    black: '#000000',
+    cobalt: '#003DB3',
+    gray: '#333333',
+    mercury: '#F7F7F7',
+    nobel: '#B3B3B3',
+    red: '#FFFFFF',
+    rose: '#FFAA80',
+    sunset: '#000000',
+    transparent: 'inherit',
+    white: '#FFFFFF',
+  },
+  fonts: {
+    small0: '10px',
+    small4: '14px',
+    small6: '16px',
+    small8: '18px',
+    medium0: '20px',
+    medium2: '22px',
+    medium4: '24px',
+    medium6: '26px',
+    medium8: '28px',
+    big0: '30px',
+  },
+  name: 'dark',
+}
+
+export default darkTheme
