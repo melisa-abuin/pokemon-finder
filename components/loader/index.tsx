07626a343db5142aa7a8b9275e858831bb45a2a2
melisa-abuin <melisa.abuin@gmail.com>
2023-08-03 22:49:39 +0200

    feature #37: add theme to loader

--- components/loader/index.tsx
+++ components/loader/index.tsx
@@ -1,11 +1,12 @@
-import { Card, Content, Image, TypeImage, TypeText } from './styles'
+import { Card, Content, Image, TypeImage, Text } from './styles'
 
 export const Loader = () => (
   <Card role="progressbar" aria-valuetext="Loading..." aria-busy="true">
+    <Text />
     <Image />
     <Content>
       <TypeImage />
-      <TypeText />
+      <Text />
     </Content>
   </Card>
 )
--- components/loader/styles.ts
+++ components/loader/styles.ts
@@ -8,17 +8,24 @@ const shine = keyframes`
 
 export const Background = styled.div`
   background: #eee;
-  background: linear-gradient(110deg, #ececec 8%, #f5f5f5 18%, #ececec 33%);
+  background: linear-gradient(
+    110deg,
+    ${({ theme }) => theme.backgrounds.primary} 8%,
+    ${({ theme }) => theme.backgrounds.tertiary} 18%,
+    ${({ theme }) => theme.backgrounds.primary} 33%
+  );
   border-radius: 5px;
   background-size: 200% 100%;
   animation: 1.5s ${shine} linear infinite;
 `
 
 export const Card = styled.div`
-  background: #fff;
-  border-radius: 5px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.05);
-  margin: 10px;
+  background-color: ${({ theme }) => theme.backgrounds.secondary};
+  border-radius: 7px;
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 2px 0px;
+  margin: 20px;
+  padding: 10px;
+  position: relative;
   width: 270px;
 `
 export const Content = styled.div`
@@ -26,14 +33,11 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  padding: 20px 30px;
+  padding: 40px 30px 20px 30px;
 `
 export const Image = styled(Background)`
-  border-bottom-left-radius: 0;
-  border-bottom-right-radius: 0;
-  border-top-left-radius: 5px;
-  border-top-right-radius: 5px;
-  height: 170px;
+  border-radius: 7px;
+  height: 120px;
   max-width: 100%;
 `
 export const TypeImage = styled(Background)`
@@ -41,8 +45,8 @@ export const TypeImage = styled(Background)`
   height: 40px;
   width: 40px;
 `
-export const TypeText = styled(Background)`
+export const Text = styled(Background)`
   height: 15px;
-  margin-top: 6px;
+  margin: 5px 0;
   width: 40px;
 `
